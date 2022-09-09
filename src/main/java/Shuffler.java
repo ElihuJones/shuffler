0a9e64c28cb7927b1ece69968f75344336fc4028
ElihuJones <xelijone@amazon.com>
2022-09-09 17:48:33 -0400

    Initial ShufflerTest commit.

---
+++ src/main/java/Shuffler.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Shuffler {                            //Class
+
+  private final Random rng;                        //Field
+
+  public Shuffler(Random rng) {                    //Constructor
+    this.rng = rng;
+  }
+  public void shuffle(int[] data) {                //Method
+    for (int i = data.length - 1; i >= 1; i--) {
+      int t = rng.nextInt(i+1);                    //Uniformly distributed random generator
+      if (t != i) {
+        int temp = data[t];                        // Swap elements in an array....Step 1
+        data[t] = data[i];                         // Swap....Step 2
+        data[i] = temp;                            // Swap....Step 3
+
+      }
+
+    }
+  }
+}
